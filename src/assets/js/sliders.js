ea4631902181a439f2fd0829498a49a2d81d9700
Ilya Stolyar <ilya22@inbox.ru>
2024-09-05 17:23:55 +0300

    add swiper slider to object image

--- src/assets/js/sliders.js
+++ src/assets/js/sliders.js
@@ -1,33 +1,10 @@
 $(document).ready(function() {
 
 
-    /*const exampleSlider = new Swiper(".example .swiper", {
-        slidesPerView: 2,
-        spaceBetween: 10,
+    const exampleSlider = new Swiper(".objects__item .swiper", {
+        slidesPerView: 1,
         observer: true,
         observeParents: true,
-
-        pagination: {
-            el: '.example .swiper-pagination',
-            clickable: true
-        },
-
-        navigation: {
-            nextEl: '.example .slider-arrow--next',
-            prevEl: '.example .slider-arrow--prev',
-        },
-
-        breakpoints: {
-            744: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-            },
-
-            1560: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-            }
-        }
-    });*/
+    });
 
 });
